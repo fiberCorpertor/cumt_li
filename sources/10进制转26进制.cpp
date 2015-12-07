d01a9a0ad0de989c4c89ce08a4ffa2c29e20ddce
fiberCorpertor <806016738@qq.com>
2015-12-07 17:52:08 +0800

    Create 10进制转26进制.cpp

---
+++ sources/10进制转26进制.cpp
@@ -0,0 +1,14 @@
+// 注意这里的减1操作
+
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string ret = "";
+        while(n)
+        {
+            ret = (char)((n-1)%26+'A') + ret;
+            n = (n-1)/26;
+        }
+        return ret;
+    }
+};
