4f255331e427f4bba6a619dc4ed61b3a29e0f4ed
fiberCorpertor <806016738@qq.com>
2015-12-08 14:51:51 +0800

    Update 10进制转26进制.cpp

--- sources/10进制转26进制.cpp
+++ sources/10进制转26进制.cpp
@@ -6,8 +6,8 @@ public:
         string ret = "";
         while(n)
         {
-            ret = (char)((n-1)%26+'A') + ret;
-            n = (n-1)/26;
+            ret = (char)((n-1) % 26 + 'A') + ret;
+            n = (n-1) / 26;
         }
         return ret;
     }
