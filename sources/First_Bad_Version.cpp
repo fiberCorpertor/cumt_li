89c930e6a6b3178b8764a3f9ce904e1c50151723
fiberCorpertor <806016738@qq.com>
2015-12-21 15:37:34 +0800

    Create First_Bad_Version.cpp
    You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
    
    Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
    
    You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.

---
+++ sources/First_Bad_Version.cpp
@@ -0,0 +1,19 @@
+// 尤其需要注意的一点：如果你把ver = (low + high)/2; 那就会出现超时错误，因为直接相加可能溢出，
+// 另外，else 处是low = ver + 1，不是low = ver
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        int ver = 0;
+        while (low < high) {
+            ver = low + (high - low) / 2;
+            if (isBadVersion(ver)) {
+                high = ver;
+            } else {
+                low = ver + 1;
+            }
+        }
+        return low;
+    }
+};
